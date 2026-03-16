f68af895ae7bf43c80e1a315b235c799c6888171
ARCgroupPVT <219491010+ARCgroupPVT@users.noreply.github.com>
2026-03-17 00:12:15 +0530

    Initial commit

---
+++ dating.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+
+daysInWeek = 7
+weeksInYears = 52
+
+today = datetime.now()
+
+sunday_this_week = today + timedelta(days=(6 - today.weekday()))
+
+sunday_at_end = sunday_this_week - timedelta(days=daysInWeek)
+# Start from the first Sunday of 2025
+sunday_at_start = datetime(2025, 1, 5)
+
+
